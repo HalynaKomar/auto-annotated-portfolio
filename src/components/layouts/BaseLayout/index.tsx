a9d08224e07cb9ddd3999d083d29950089ecdd49
HalynaKomar <124254423+HalynaKomar@users.noreply.github.com>
2025-03-04 15:34:16 +0200

    Update index.tsx

--- src/components/layouts/BaseLayout/index.tsx
+++ src/components/layouts/BaseLayout/index.tsx
@@ -13,10 +13,9 @@ const BaseLayout: React.FC = () => {
             <Head>
                 <title>My Image</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+
 <script async src="https://stage-widget.intelswift.com/script.js?tenantId=835c3375-8bff-4f6b-94d0-dbbd537f5303&botId=67c6f37e01b3e636a11e4eed&uuid=70907f9c-aaec-43dc-a0d4-7d82108c376a&end=true"></script>
-                  <script
-  dangerouslySetInnerHTML={{
-    __html: `
+<script>
   window.onload = (event) => {
     const propsInterval = setInterval(widgetTimer, 1000);
 
@@ -45,9 +44,8 @@ const BaseLayout: React.FC = () => {
       }
     }
   };
-    `
-  }}
-/>
+</script>
+                
 
             </Head>
             <img src="/my-image.jpg" alt="Custom Image" style={{ maxWidth: '100%', height: 'auto' }} />
